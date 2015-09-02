43873a28fbdb77f584207654c6497d40cb3ded6b
unknown <kwekut@gmail.com>
2015-09-02 18:31:04 -0400

    make it better

--- app/utils/PlayGlobalSettings.scala
+++ app/utils/PlayGlobalSettings.scala
@@ -7,27 +7,23 @@ import jdub.async.Database
 import org.joda.time.DateTimeZone
 import play.api.{ Application, GlobalSettings }
 import services.database.Schema
+import play.api.Logger
 
 object PlayGlobalSettings extends GlobalSettings {
   override def onStart(app: Application) = {
     DateTimeZone.setDefault(DateTimeZone.UTC)
     TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
 
-    val cnf = play.api.Play.current.configuration
+    val defurl = play.api.Play.current.configuration.getString("db.default.url").get
 
-
-    val dbUri = new URI(cnf.get("db.default.url"))
+    val dbUri = new URI(defurl)
     val host = dbUri.getHost()
     val port = dbUri.getPort()
-    val database = dbUri.getPath()
-    val username = dbUri.getUserInfo().split(":")[0];
-    val password = dbUri.getUserInfo().split(":")[1];
+    val database = Some(dbUri.getPath().stripPrefix("/"))
+    val username = dbUri.getUserInfo().split(":")(0)
+    val pword = dbUri.getUserInfo().split(":")(1)
+    val password = Some(pword)
 
-    // val host = cnf.getString("db.host").getOrElse("localhost")
-    // val port = 5432
-    // val database = cnf.getString("db.database")
-    // val username = cnf.getString("db.username").getOrElse("silhouette")
-    // val password = cnf.getString("db.password")
 
     Database.open(username, host, port, password, database)
     Schema.update()
